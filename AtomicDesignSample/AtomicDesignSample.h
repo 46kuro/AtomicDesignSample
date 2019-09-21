8b9459cdc470726a4d18752bb4fe4ab366b0884d
shinji46kuro <shinji.46.kuro@gmail.com>
2019-09-21 20:41:02 +0900

    Initial Commit

---
+++ AtomicDesignSample/AtomicDesignSample.h
@@ -0,0 +1,19 @@
+//
+//  AtomicDesignSample.h
+//  AtomicDesignSample
+//
+//  Created by Shinji Kurosawa on 2019/09/21.
+//  Copyright © 2019 Shinji Kurosawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AtomicDesignSample.
+FOUNDATION_EXPORT double AtomicDesignSampleVersionNumber;
+
+//! Project version string for AtomicDesignSample.
+FOUNDATION_EXPORT const unsigned char AtomicDesignSampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AtomicDesignSample/PublicHeader.h>
+
+
