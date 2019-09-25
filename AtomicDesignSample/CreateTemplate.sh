f4981eac3bdb6b62ae94d8650d2ee942b743ff0b
shinji46kuro <shinji.46.kuro@gmail.com>
2019-09-25 23:28:35 +0900

    Template作成用のShellの流れ記述

---
+++ AtomicDesignSample/CreateTemplate.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+#  CreateTemplate.sh
+#  AtomicDesignSample
+#
+#  Created by Shinji Kurosawa on 2019/09/25.
+#  Copyright © 2019 Shinji Kurosawa. All rights reserved.
+
+
+#while getopts ab:c: OPT
+#do
+#case $OPT in
+#    "p" ) ;;
+#exit 1 ;;
+#esac
+#done
+
+# TODO: GenerambaでTemplate作成
+
+# TODO: 仮定義用の変数名をclass内に定義
+
+# TODO: sourcery --sources Molecules/SampleButtonsView.swift --templates Sample.stencil --output Molecules/SampleButtonsView.swift
+
+# TODO: 仮定義用の変数名を削除
