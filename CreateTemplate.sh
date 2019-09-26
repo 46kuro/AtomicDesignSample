4a70603336e2e586ad53a55ebb26a158cbceffac
shinji46kuro <shinji.46.kuro@gmail.com>
2019-09-26 14:57:50 +0900

    GenerambaのModule作成まで

--- CreateTemplate.sh
+++ CreateTemplate.sh
@@ -7,15 +7,19 @@
 #  Copyright © 2019 Shinji Kurosawa. All rights reserved.
 
 
-#while getopts ab:c: OPT
-#do
-#case $OPT in
-#    "p" ) ;;
-#exit 1 ;;
-#esac
-#done
+while getopts n:p: OPT
+do
+case $OPT in
+    "n" ) MOLECULE_NAME="$OPTARG";;
+    "p" ) PARAMETERS="$OPTARG";;
+esac
+done
 
+echo $MOLECULE_NAME
+echo $PARAMETERS
 # TODO: GenerambaでTemplate作成
+#bundle exec generamba template install
+#bundle exec generamba gen Sample2 Molecules # TODO: Nameを外から受け取る
 
 # TODO: 仮定義用の変数名をclass内に定義
 
